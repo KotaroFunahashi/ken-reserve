f2d44f27697f799b2fb6bd858e44cbc8a6ee2d1a
KotaroFunahashi <gorilorris@gmail.com>
2026-02-28 03:55:27 +0900

    初回プロジェクトひな型作成

---
+++ src/main/java/com/coha9nus/kenreserve/KenReserveApplication.java
@@ -0,0 +1,13 @@
+package com.coha9nus.kenreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coha9nus/kenreserve/KenReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coha9nus.kenreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
