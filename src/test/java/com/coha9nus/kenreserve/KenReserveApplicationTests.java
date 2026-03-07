843b9007ccc7113e2b68fcabb23c69408312f5c8
remi545 <remilias545@gmail.com>
2026-03-07 16:30:51 +0900

    Testcontainers適用

--- src/test/java/com/coha9nus/kenreserve/KenReserveApplicationTests.java
+++ src/test/java/com/coha9nus/kenreserve/KenReserveApplicationTests.java
@@ -2,12 +2,13 @@ package com.coha9nus.kenreserve;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class KenReserveApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
