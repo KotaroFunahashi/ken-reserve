5f2a59c5e75c5b3cfffaa9014ad617f107e8e7f6
KotaroFunahashi <gorilorris@gmail.com>
2026-04-10 00:29:52 +0900

    DB初期ユーザの作成処理前にユーザの存在チェックを追加。

--- src/main/java/com/coha9nus/kenreserve/config/DataInitializer.java
+++ src/main/java/com/coha9nus/kenreserve/config/DataInitializer.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -23,12 +22,12 @@ public class DataInitializer implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        try {
-            String password = passwordEncoder.encode(adminPassword);
-            userRepository.save(User.builder().loginId("admin").password(password)
-                    .displayName("管理者").role(Role.ADMIN).build());
-        } catch (DataIntegrityViolationException e) {
-            // 別インスタンスが先に挿入済みの場合は正常扱い
+        // APサーバ冗長化で初期管理ユーザ作成が競合する可能性があるが、単一サーバ前提のため考慮不要
+        if (userRepository.findByLoginId("admin").isPresent()) {
+            return;
         }
+        String password = passwordEncoder.encode(adminPassword);
+        userRepository.save(User.builder().loginId("admin").password(password).displayName("管理者")
+                .role(Role.ADMIN).build());
     }
 }
